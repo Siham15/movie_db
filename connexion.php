98f7ad96c2931fec0d8a400f61154f01aea9449e
Siham <siham.hakem@gmail.com>
2022-02-07 18:14:19 +0100

    Projet de base

---
+++ connexion.php
@@ -0,0 +1,8 @@
+<?php
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
